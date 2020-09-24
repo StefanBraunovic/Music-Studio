249353848e060ddb84efe723a2e9d8dee878ac7a
Stefan Braunovic <stefan.braunovic55@gmail.com>
2020-09-24 15:39:16 +0200

    Color,Gallery

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
-	if (scroll_position >100) {
+	if (scroll_position > 100) {
 		header.style.backgroundColor = '#485563';
 	} else {
 		header.style.backgroundColor = 'transparent';
@@ -23,3 +23,5 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+
